c392b75aa52a8c7a68ddc8a673ec09a49cbf3c5d
Austin King <aust.a.king@gmail.com>
2015-09-19 19:38:31 -0700

    Added functionality to have users and authorize them

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/char_sheet_dev');
+process.env.APP_SECRET = process.env.APP_SECRET || 'changemechangemechangeme';
 
-var charRouter = require(__dirname + '/routes/char_sheets_router.js');
+var charRouter = require(__dirname + '/routes/char_sheets_router');
 app.use('/api', charRouter);
+var userRouter = require(__dirname + '/routes/user_router');
+app.use('/api', userRouter);
 
 app.listen(3000, function() {
   console.log("Server listening...");
