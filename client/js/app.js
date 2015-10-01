de67d42d1de9e54619531903c8ceffafcbf154cd
Austin King <aust.a.king@gmail.com>
2015-09-30 22:21:50 -0700

    Added jshint task to gulpfile and ran it.

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 require('angular/angular');
 
-var charSheetApp = angular.module('charSheetApp', []);
+var charSheetApp = angular.module('charSheetApp', []);  /* jshint ignore: line */
 
 charSheetApp.controller('CharSheetController', ['$scope', function($scope) {
   $scope.description = 'Create a character sheet';
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var webpack = require('webpack-stream');
+
+gulp.task('jshint', function() {
+  return gulp.src(['test/**/*.js', 'lib/**/*.js', 'models/**/*', 'routes/**/*', 'server.js', 'gulpfile.js', 'client/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
 
 gulp.task('webpack:dev', function() {
   return gulp.src('./client/js/app.js')
@@ -13,10 +20,11 @@ gulp.task('webpack:dev', function() {
 });
 
 gulp.task('staticfiles:dev', function() {
+  gulp.watch('./client/**/*.html', ['staticfiles:dev']);
+  
   return gulp.src('./client/**/*.html')
     .pipe(gulp.dest('build/'));
 });
 
-gulp.watch('./client/**/*.html', ['staticfiles:dev']);
 gulp.task('build:dev', ['staticfiles:dev', 'webpack:dev']);
 gulp.task('default', ['build:dev']);
