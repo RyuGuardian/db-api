4970d909c301bcb69bd772d33baef99b61f5bdb3
Austin King <aust.a.king@gmail.com>
2015-10-02 23:45:11 -0700

    GET and POST tests work.

--- test/client/entry.js
+++ test/client/entry.js
@@ -1,1 +1,2 @@
+//because gulp only brings in one file, this requires in all test files.
 require(__dirname + '/notes_http_test.js');
--- test/client/notes_http_test.js
+++ test/client/notes_http_test.js
@@ -19,4 +19,33 @@ describe('character-sheet controller', function() {
     expect(typeof controller).toBe('object');
     expect(Array.isArray($scope.sheets)).toBe(true);
   });
+
+  describe('REST requests', function() {
+    beforeEach(angular.mock.inject(function(_$httpBackend_, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      $scope = $rootScope.$new();
+      $ControllerConstructor('CharSheetsController', {$scope: $scope});
+    }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should make a GET request when getAll() is called', function() {
+      $httpBackend.expectGET('/api/sheets').respond(200, [{name: 'TESTER'}]);
+      $scope.getAll();
+      $httpBackend.flush();
+      expect($scope.sheets[0].name).toBe('TESTER');
+    });
+
+    it('should be able to create a note', function() {
+      $httpBackend.expectPOST('/api/sheets', {name: 'TESTER3'}).respond(200, {_id: 1, name: 'TESTER2'});
+      $scope.newSheet = {name: 'HELLO'};
+      $scope.createCharacterSheet({name: 'TESTER3'});
+      $httpBackend.flush();
+      expect($scope.sheets[0].name).toBe('TESTER2');
+      expect($scope.newSheet).toBe(null);
+    });
+  });
 });
