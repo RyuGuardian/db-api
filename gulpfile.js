3dae3118726317ff8e8dd3ce4b8c9819343c5027
Austin King <aust.a.king@gmail.com>
2015-12-01 11:21:53 -0800

    Build for deployment.

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,49 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var Karma = require('karma').Server;
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
 
-if(process.env.NODE_ENV !== 'production') {
-  var mocha = require('gulp-mocha');
-  var jshint = require('gulp-jshint');
+gulp.task('jshint', function() {
+  return gulp.src(['test/**/*.js', 'lib/**/*.js', 'models/**/*', 'routes/**/*', 'server.js', 'gulpfile.js', 'client/**/*.js'])
+    .pipe(jshint({
+      node: true,
+      globals: {
+        angular: true,
+        expect: true
+      }
+    }))
+    .pipe(jshint.reporter('default'));
+});
 
-  gulp.task('jshint', function() {
-    return gulp.src(['test/**/*.js', 'lib/**/*.js', 'models/**/*', 'routes/**/*', 'server.js', 'gulpfile.js', 'client/**/*.js'])
-      .pipe(jshint({
-        node: true,
-        globals: {
-          angular: true,
-          expect: true
-        }
-      }))
-      .pipe(jshint.reporter('default'));
-  });
+gulp.task('webpack:test', function() {
+  return gulp.src('./test/client/entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'test_bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('test/client'));
+});
 
-  gulp.task('webpack:test', function() {
-    return gulp.src('./test/client/entry.js')
-      .pipe(webpack({
-        output: {
-          filename: 'test_bundle.js'
-        }
-      }))
-      .pipe(gulp.dest('test/client'));
-  });
+gulp.task('karmatests', ['webpack:test'], function(done) {
+  new Karma({configFile: __dirname + '/karma.conf.js'}, done)
+    .start();
+});
 
-  gulp.task('karmatests', ['webpack:test'], function(done) {
-    new Karma({configFile: __dirname + '/karma.conf.js'}, done)
-      .start();
-  });
-
-  gulp.task('servertests', function() {
-    return gulp.src('test/api/**/*test.js')
-      .pipe(mocha())
-      .once('error', function(err) {
-        console.log(err)
-        process.exit(1);
-      })
-      .once('end', function() {
-        if(this.seq.length === 1 && this.seq[0] === 'servertests') {
-          process.exit();
-        }
-      }.bind(this));
-  });
-}
+gulp.task('servertests', function() {
+  return gulp.src('test/api/**/*test.js')
+    .pipe(mocha())
+    .once('error', function(err) {
+      console.log(err)
+      process.exit(1);
+    })
+    .once('end', function() {
+      if(this.seq.length === 1 && this.seq[0] === 'servertests') {
+        process.exit();
+      }
+    }.bind(this));
+});
 
 gulp.task('webpack:dev', function() {
   return gulp.src('./client/js/app.js')
@@ -68,7 +65,7 @@ gulp.task('staticfiles:dev', function() {
 
 gulp.task('staticfiles:prod', function() {
   return gulp.src('./client/**/*.html')
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest('frontend/'));
 });
 
 gulp.task('webpack:prod', function() {
@@ -78,9 +75,9 @@ gulp.task('webpack:prod', function() {
         filename: 'bundle.js'
       }
     }))
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest('frontend/'));
 });
 
 gulp.task('build:dev', ['staticfiles:dev', 'webpack:dev']);
-gulp.task('build', ['staticfiles:prod', 'webpack:prod']);
+gulp.task('build:prod', ['staticfiles:prod', 'webpack:prod']);
 gulp.task('default', ['build:dev']);
